0f1e4ccfa342606a4d9c42acc52796da2eb79ad0
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-08 21:45:54 -0300

    Consumer Parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,13 +36,16 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	@Override
@@ -50,28 +54,9 @@ public class MainViewController implements Initializable{
 		
 	}
 	
-	
-	private synchronized void loadView(String absoluteName) { // Esse synchronized eh feito para que esse processo nao seja interrompido na execucao das threads. 
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();	
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-		} catch(IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-		
-	}
-	
-	
-	private synchronized void loadView2(String absoluteName) { // Esse synchronized eh feito para que esse processo nao seja interrompido na execucao das threads. 
+	// fizemos esse consumer para nao precisarmos fazer outras funcoes, que diferem uma ou outra linha
+																  // Parametrizar uma funcao
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) { // Esse synchronized eh feito para que esse processo nao seja interrompido na execucao das threads. 
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();	
@@ -85,18 +70,11 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			DepartmentListController controller = loader.getController();
-			
-			controller.setDepartmentService(new DepartmentService());
-			
-			controller.updateTableView();
-			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		} catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 		
 	}
-	
-	
-
 }
