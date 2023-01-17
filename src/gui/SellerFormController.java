12d8a3aa39be1fa8ab2ddf928f7715cccf03cd11
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-16 22:28:27 -0300

    TextField & DatePicker

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,181 @@
+package gui;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.exceptions.ValidationException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+
+	private Seller entity;
+
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListener = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txtBaseSalary;
+	
+	@FXML
+	private javafx.scene.control.Label labelErrorName;
+	
+	@FXML
+	private javafx.scene.control.Label labelErrorEmail;
+	
+	@FXML
+	private javafx.scene.control.Label labelErrorBirthDate;
+	
+	@FXML
+	private javafx.scene.control.Label labelErrorBaseSalary;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML 
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListener.add(listener);
+	}
+	
+	@FXML 
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity==null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		if(service==null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+			
+		} 
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListener) {
+			listener.onDataChanged();
+		}
+	}
+	
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation Error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addErrors("name", "Field can't be empty");
+		}
+
+		obj.setName(txtName.getText());
+
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+	@FXML 
+	public void onBtCancelAction(ActionEvent event) {
+		System.out.println("onBtCancelAction");
+		Utils.currentStage(event).close();
+
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+	}
+	
+	public void updateFormData() {
+		
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		// Convert the date to localDate
+		if(entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));	
+		}
+		
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+
+}
